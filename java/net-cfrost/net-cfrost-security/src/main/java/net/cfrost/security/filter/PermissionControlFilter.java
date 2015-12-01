a7e2805fc482e261eeab28eca27a9a89c6c47608
cFrost <cFrost.sun@Live.com>
2015-12-01 11:56:38 +0800

    Add util module

--- java/net-cfrost/net-cfrost-security/src/main/java/net/cfrost/security/filter/PermissionControlFilter.java
+++ java/net-cfrost/net-cfrost-security/src/main/java/net/cfrost/security/filter/PermissionControlFilter.java
@@ -10,24 +10,13 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-public class PermissionControlFilter implements Filter {
+import net.cfrost.util.web.UrlTool;
 
-    private String[] webFolders;
-    
-    public void setWebFolder(String webFolder) {
-        this.webFolders = webFolder.trim().split(",");
-    }
-    
-    public PermissionControlFilter() {}
-    
-    public PermissionControlFilter(String webFolder) {
-        this.webFolders = webFolder.trim().split(",");
-    }
+public class PermissionControlFilter implements Filter {
 
     @Override
     public void init(FilterConfig paramFilterConfig) throws ServletException {
         // TODO Auto-generated method stub
-        this.webFolders=paramFilterConfig.getInitParameter("webFolder").split(",");
     }
 
     @Override
@@ -35,17 +24,11 @@ public class PermissionControlFilter implements Filter {
             ServletResponse paramServletResponse, FilterChain paramFilterChain)
             throws IOException, ServletException {
         // TODO Auto-generated method stub
-        String requestURI = ((HttpServletRequest)paramServletRequest).getRequestURI();
-        String contextPath = ((HttpServletRequest)paramServletRequest).getContextPath();
-        
-        //去掉项目名称后的URI
-        String realURI = requestURI.replaceFirst(contextPath, "");
         
         //模块名称 /xxx/yyy/zzz中的xxx
-        String moduleName = this.findModuleName(realURI);
-        
-        System.out.println("realURI:"+realURI);
-        System.out.println("moduleName:"+moduleName);
+        String moduleName = UrlTool.findModuleName((HttpServletRequest)paramServletRequest);
+
+        System.out.println("PermissionControl:Module Name is "+moduleName);
         paramFilterChain.doFilter(paramServletRequest, paramServletResponse);
     }
 
@@ -53,25 +36,5 @@ public class PermissionControlFilter implements Filter {
     public void destroy() {
         // TODO Auto-generated method stub
         
-    }
-
-    private String findModuleName(String URI) {
-        String moduleName = null;
-        
-        if(URI.startsWith("/"))
-            URI = URI.replaceFirst("/", "");
-        
-        for(String webFolder : this.webFolders){
-            if(URI.startsWith(webFolder)) {
-                URI = URI.replaceFirst(webFolder+"/", "");
-                break;
-            }
-        }
-        
-        if(URI.indexOf("/") < 0)
-            moduleName = "/";
-        else
-            moduleName = URI.substring(0, URI.indexOf("/"));
-        return moduleName;
     }
 }
