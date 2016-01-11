d84c82564d87c3be47e9ac677dbb6a4c0010c0c5
cFrost <cFrost@10.88.5.166>
2016-01-11 17:15:39 +0800

    some changes

--- java/net-cfrost/net-cfrost-web/src/main/webapp/scripts/util.js
+++ java/net-cfrost/net-cfrost-web/src/main/webapp/scripts/util.js
@@ -1,10 +1,10 @@
 var browserName = navigator.appName;
 
-var strFullPath=window.document.location.href;
-var strPath=window.document.location.pathname;
-var pos=strFullPath.indexOf(strPath);
-var prePath=strFullPath.substring(0,pos);
-var postPath=strPath.substring(0,strPath.substr(1).indexOf('/')+1);
+//var strFullPath=window.document.location.href;
+//var strPath=window.document.location.pathname;
+//var pos=strFullPath.indexOf(strPath);
+//var prePath=strFullPath.substring(0,pos);
+//var postPath=strPath.substring(0,strPath.substr(1).indexOf('/')+1);
 //alert(prePath+postPath);
 
 if(browserName != "Netscape") {
